af51c0b53408dd81d05e421ef28aba1960429d49
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2022-08-10 17:08:48 +0700

    Welcoming BetaBar into Cloudflare Workers

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,20 @@
 <script setup>
+import {loadFull} from "tsparticles";
+const particlesInit = async (engine) => {
+  await loadFull(engine);
+}
+
+const particlesLoaded = async (container) => {
+  console.log("Particles container loaded", container);
+}
 </script>
 <template>
+  <Particles
+      id="bgParticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="configVal"
+  />
   <div class="customContainer">
     <slot/>
   </div>
@@ -8,7 +22,48 @@
 
 <script>
 export default {
-  name: "default"
+  name: "default",
+  props:["speed"],
+  data(){
+    return {
+      configVal: {
+        detectRetina: true,
+        fpsLimit: 120,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            resize: true
+          }
+        },
+        particles: {
+          color: {
+            value: "#fff"
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1080
+            },
+            limit: 0,
+            value: 200
+          },
+          opacity: {
+            animation: {
+              enable: true,
+              minimumValue: 0.05,
+              speed: 0.25,
+              sync: false
+            },
+            random: {
+              enable: true,
+              minimumValue: 0.05
+            },
+            value: 1
+          }
+        }
+      }
+    }
+  }
 }
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,5 +23,5 @@ export default defineNuxtConfig({
         download: true,
         display: 'auto',
     },
-    "target" : "static"
+    target: "static"
 })
